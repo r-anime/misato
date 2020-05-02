3b9b0d67a0014459c30bbdd19ec02db2f3eff343
George <georgej1088@gmail.com>
2020-05-01 20:07:35 -0400

    Reformat redditAuth.js

--- web/routes/redditAuth.js
+++ web/routes/redditAuth.js
@@ -16,38 +16,41 @@ const redditAuthURIBase = 'https://old.reddit.com/api/v1/authorize'
 	+ '&duration=permanent';
 /* eslint-enable */
 
-// Generates an auth URI to redirect the user to given a state
+/**
+ * Generates an auth URI to redirect the user to given a state.
+ * @param {string} state
+ * @returns {String}
+ */
 function authURI (state) {
 	return `${redditAuthURIBase}&state=${encodeURIComponent(state)}`;
 }
-const redditAuth = polka();
 
-redditAuth.get('/', (request, response) => {
-	const state = `${Math.random()}`; // TODO: this should be secure
-	request.session.state = state;
-	response.redirect(authURI(state));
-});
+module.exports = polka()
 
-redditAuth.get('/callback', (request, response) => {
-	const {error, state, code} = request.query;
+	.get('/', (request, response) => {
+		const state = `${Math.random()}`; // TODO: this should be secure
+		request.session.state = state;
+		response.redirect(authURI(state));
+	})
 
-	// Check for errors or state mismatches
-	if (error) {
-		log.error('Error in auth flow:', error);
-		response.end('uh-oh');
-		process.exit(1);
-	}
-	if (state !== request.session.state) {
-		log.error('State mismatch in auth flow: session had', request.session.state, '; reddit gave', state);
-		response.end('uh-oh');
-		process.exit(1);
-	}
+	.get('/callback', (request, response) => {
+		const {error, state, code} = request.query;
 
-	// Get the access token from Reddit
-	// TODO
-	log.info(code);
-	request.session.redditCode = code; // very temporary
-	response.redirect('/');
-});
+		// Check for errors or state mismatches
+		if (error) {
+			log.error('Error in auth flow:', error);
+			response.end('uh-oh');
+			process.exit(1);
+		}
+		if (state !== request.session.state) {
+			log.error('State mismatch in auth flow: session had', request.session.state, '; reddit gave', state);
+			response.end('uh-oh');
+			process.exit(1);
+		}
 
-module.exports = redditAuth;
+		// Get the access token from Reddit
+		// TODO
+		log.info(code);
+		request.session.redditCode = code; // very temporary
+		response.redirect('/');
+	});
