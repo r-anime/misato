42d883c46432e5b7ebf38f026a22efeb4f95cdd5
George <georgej1088@gmail.com>
2020-06-06 02:42:35 -0400

    Remove useless wrapping async IIFE

--- web/index.js
+++ web/index.js
@@ -8,55 +8,53 @@ const config = require('../config');
 const responseHelpers = require('./middleware/responseHelpers');
 const auth = require('./routes/auth');
 
-(async () => {
-	// Set up our app
-	const app = polka();
+// Set up our app
+const app = polka();
 
-	// Set up session storage, delegated to parent process via IPC
-	app.use(session({
-		store: new IPCStore(),
-		secret: config.web.sessionSecret,
-		saveUninitialized: false,
-		resave: false,
-	}));
+// Set up session storage, delegated to parent process via IPC
+app.use(session({
+	store: new IPCStore(),
+	secret: config.web.sessionSecret,
+	saveUninitialized: false,
+	resave: false,
+}));
 
-	// Set up our other middlewares
-	app.use(responseHelpers);
+// Set up our other middlewares
+app.use(responseHelpers);
 
-	// Register sub-apps for different routes
-	app.use('/auth', auth);
+// Register sub-apps for different routes
+app.use('/auth', auth);
 
-	// very temporary testing route
-	app.get('/', (request, response) => {
-		response.end(`
-			<!DOCTYPE html>
-			<html>
-			<head>
-				<title>dab on em</title>
-				<meta charset="utf-8">
-			</head>
-			<body>
-				${request.session.redditUserInfo ? `
-					<p>logged into reddit as ${request.session.redditUserInfo.name} (<a href="/auth/reddit/logout">log out</a>)</p>
-				` : `
-					<p>not logged in with reddit, <a href="/auth/reddit">go log in</a></p>
-				`}
-				${request.session.discordUserInfo ? `
-					<p>logged into discord as @${request.session.discordUserInfo.username}#${request.session.discordUserInfo.discriminator} (<a href="/auth/discord/logout">log out</a>)</p>
-				` : `
-					<p>not logged in with discord, <a href="/auth/discord">go log in</a></p>
-				`}
-				<p>oauth providers pls dont xss my website kthx</p>
-			</body></html>
-		`);
-	});
+// very temporary testing route
+app.get('/', (request, response) => {
+	response.end(`
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>dab on em</title>
+			<meta charset="utf-8">
+		</head>
+		<body>
+			${request.session.redditUserInfo ? `
+				<p>logged into reddit as ${request.session.redditUserInfo.name} (<a href="/auth/reddit/logout">log out</a>)</p>
+			` : `
+				<p>not logged in with reddit, <a href="/auth/reddit">go log in</a></p>
+			`}
+			${request.session.discordUserInfo ? `
+				<p>logged into discord as @${request.session.discordUserInfo.username}#${request.session.discordUserInfo.discriminator} (<a href="/auth/discord/logout">log out</a>)</p>
+			` : `
+				<p>not logged in with discord, <a href="/auth/discord">go log in</a></p>
+			`}
+			<p>oauth providers pls dont xss my website kthx</p>
+		</body></html>
+	`);
+});
 
-	// Start the server
-	app.listen(config.web.port, error => {
-		if (error) {
-			log.error(error);
-			process.exit(1);
-		}
-		log.success(`Server listening on port ${config.web.port}`);
-	});
-})();
+// Start the server
+app.listen(config.web.port, error => {
+	if (error) {
+		log.error(error);
+		process.exit(1);
+	}
+	log.success(`Server listening on port ${config.web.port}`);
+});
