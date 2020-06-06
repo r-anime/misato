12e6270ccf9cf7f3126c951ecda6849fe30fde2f
George <georgej1088@gmail.com>
2020-06-06 14:45:00 -0400

    yeah so IPCStore was a mistake

--- web/index.js
+++ web/index.js
@@ -1,60 +1,71 @@
 const polka = require('polka');
 
 const log = require('another-logger')({label: 'web'});
+const {MongoClient} = require('mongodb');
 const session = require('express-session');
-const IPCStore = require('../util/IPCStore')(session);
+const MongoStore = require('connect-mongo')(session);
 
 const config = require('../config');
 const responseHelpers = require('./middleware/responseHelpers');
 const auth = require('./routes/auth');
 
-// Set up our app
-const app = polka();
+(async () => {
+	// Set up our app
+	const app = polka();
 
-// Set up session storage, delegated to parent process via IPC
-app.use(session({
-	store: new IPCStore(),
-	secret: config.web.sessionSecret,
-	saveUninitialized: false,
-	resave: false,
-}));
+	// Set up MongoDB
+	const mongoClient = new MongoClient(config.mongodb.url, {useUnifiedTopology: true});
+	await mongoClient.connect();
+	log.success('Connected to MongoDB on', config.mongodb.url);
 
-// Set up our other middlewares
-app.use(responseHelpers);
+	// Set up session storage, delegated to parent process via IPC
+	app.use(session({
+		store: new MongoStore({
+			client: mongoClient,
+			dbName: config.mongodb.databaseName,
+		}),
+		secret: config.web.sessionSecret,
+		saveUninitialized: false,
+		resave: false,
+	}));
 
-// Register sub-apps for different routes
-app.use('/auth', auth);
+	// Set up our other middlewares
+	app.use(responseHelpers);
 
-// very temporary testing route
-app.get('/', (request, response) => {
-	response.end(`
-		<!DOCTYPE html>
-		<html>
-		<head>
-			<title>dab on em</title>
-			<meta charset="utf-8">
-		</head>
-		<body>
-			${request.session.redditUserInfo ? `
-				<p>logged into reddit as ${request.session.redditUserInfo.name} (<a href="/auth/reddit/logout">log out</a>)</p>
-			` : `
-				<p>not logged in with reddit, <a href="/auth/reddit">go log in</a></p>
-			`}
-			${request.session.discordUserInfo ? `
-				<p>logged into discord as @${request.session.discordUserInfo.username}#${request.session.discordUserInfo.discriminator} (<a href="/auth/discord/logout">log out</a>)</p>
-			` : `
-				<p>not logged in with discord, <a href="/auth/discord">go log in</a></p>
-			`}
-			<p>oauth providers pls dont xss my website kthx</p>
-		</body></html>
-	`);
-});
+	// Register sub-apps for different routes
+	app.use('/auth', auth);
 
-// Start the server
-app.listen(config.web.port, error => {
-	if (error) {
-		log.error(error);
-		process.exit(1);
-	}
-	log.success(`Server listening on port ${config.web.port}`);
-});
+	// very temporary testing route
+	app.get('/', (request, response) => {
+		response.end(`
+			<!DOCTYPE html>
+			<html>
+			<head>
+				<title>dab on em</title>
+				<meta charset="utf-8">
+			</head>
+			<body>
+				${request.session.redditUserInfo ? `
+					<p>logged into reddit as ${request.session.redditUserInfo.name} (<a href="/auth/reddit/logout">log out</a>)</p>
+				` : `
+					<p>not logged in with reddit, <a href="/auth/reddit">go log in</a></p>
+				`}
+				${request.session.discordUserInfo ? `
+					<p>logged into discord as @${request.session.discordUserInfo.username}#${request.session.discordUserInfo.discriminator} (<a href="/auth/discord/logout">log out</a>)</p>
+				` : `
+					<p>not logged in with discord, <a href="/auth/discord">go log in</a></p>
+				`}
+				<p>oauth providers pls dont xss my website kthx</p>
+			</body></html>
+		`);
+	});
+
+	// Start the server
+	app.listen(config.web.port, error => {
+		if (error) {
+			log.error(error);
+			process.exit(1);
+		}
+		log.success(`Server listening on port ${config.web.port}`);
+	});
+})();
