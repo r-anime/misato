ac5e14552303109fa44612473baafe750f345767
bernasx <bernardo.ribeiro99@gmail.com>
2020-06-20 17:50:29 +0100

    Using async/await properly now, removed gitignored database folder

--- bot/commands/joke.js
+++ bot/commands/joke.js
@@ -5,14 +5,12 @@ const {Command} = require('yuuko');
 const fetch = require('node-fetch');
 
 module.exports = new Command('joke', async msg => {
-	await fetch('https://official-joke-api.appspot.com/random_joke')
-		.then(res => res.json())
-		.then(data => {
-			msg.channel.createMessage(`${data.setup}\n${data.punchline}`);
-		});
+	const res = await fetch('https://official-joke-api.appspot.com/random_joke');
+	const joke = await res.json();
+	msg.channel.createMessage(`${joke.setup}\n${joke.punchline}`);
 });
 
 module.exports.help = {
 	args: '',
-	desc: 'Fetches a random joke from https://github.com/15Dkatz/official_joke_api',
+	desc: 'Fetches a random joke from <https://github.com/15Dkatz/official_joke_api>',
 };
