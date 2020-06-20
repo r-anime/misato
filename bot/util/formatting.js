2e72780aea609e171259dcce9fa34abcec39be89
George <georgej1088@gmail.com>
2020-06-20 04:09:23 -0400

    testing command for adding new content containsText filters

--- bot/util/formatting.js
+++ bot/util/formatting.js
@@ -12,4 +12,17 @@ module.exports = {
 		return text.replace(/^|\n(?!$)/g, '$&> ');
 	},
 
+	/**
+	 * Wraps the given text in a code block of the given language. Inserts
+	 * zero-width spaces into existing sequences of triple backticks to avoid
+	 * malicious formatting, unless the unsafe argument is set to true.
+	 * @param {string} text
+	 * @param {string} [lang='']
+	 * @param {boolean} [unsafe=false]
+	 * @returns {string}
+	 */
+	code (text, lang = '', unsafe = false) {
+		return `\`\`\`${lang ? `${lang}\n` : ''}${unsafe ? text : text.replace(/```/g, '`​`​`')}\`\`\``;
+	},
+
 };
