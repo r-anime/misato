5b81bc282dc6a856d50708a5f0d8f1331ea4a768
bernasx <38398866+bernasx@users.noreply.github.com>
2020-07-05 19:36:58 +0100

    Small commands (#9)

---
+++ bot/commands/joke.js
@@ -0,0 +1,23 @@
+// Fetches a joke JSON from https://github.com/15Dkatz/official_joke_api
+// and posts it in chat for moderate amounts of fun.
+
+const {Command} = require('yuuko');
+const fetch = require('node-fetch');
+
+module.exports = new Command('joke', async msg => {
+	try {
+		const res = await fetch('https://official-joke-api.appspot.com/random_joke');
+		if (res.status !== 200) {
+			throw Error('Error getting joke.');
+		}
+		const joke = await res.json();
+		msg.channel.createMessage(`${joke.setup}\n${joke.punchline}`);
+	} catch (err) {
+		msg.channel.createMessage(err.message).catch(() => {});
+	}
+});
+
+module.exports.help = {
+	args: '',
+	desc: 'Fetches a random joke from <https://github.com/15Dkatz/official_joke_api>',
+};
