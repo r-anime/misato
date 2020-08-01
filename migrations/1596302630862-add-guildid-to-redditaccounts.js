4b0ade1e0e9895b602745fb6c34d636bfc2639a5
George <georgej1088@gmail.com>
2020-08-01 13:25:11 -0400

    migration to recreate redditaccounts table with guildid required

---
+++ migrations/1596302630862-add-guildid-to-redditaccounts.js
@@ -0,0 +1,38 @@
+const {MongoClient} = require('mongodb');
+const config = require('../config');
+
+module.exports.up = async () => {
+	const mongoClient = new MongoClient(config.mongodb.url, {useUnifiedTopology: true});
+	await mongoClient.connect();
+	const db = mongoClient.db(config.mongodb.databaseName);
+
+	await db.dropCollection('redditAccounts');
+
+	await db.createCollection('redditAccounts', {
+		validator: {
+			$jsonSchema: {
+				bsonType: 'object',
+				required: ['userID', 'guildID', 'redditName'],
+				properties: {
+					userID: {
+						bsonType: 'string',
+					},
+					guildID: {
+						bsonType: 'string',
+					},
+					redditName: {
+						bsonType: 'string',
+					},
+				},
+			},
+		},
+	});
+};
+
+module.exports.down = async () => {
+	const mongoClient = new MongoClient(config.mongodb.url, {useUnifiedTopology: true});
+	await mongoClient.connect();
+	const db = mongoClient.db(config.mongodb.databaseName);
+
+	await db.dropCollection('redditAccounts');
+};
