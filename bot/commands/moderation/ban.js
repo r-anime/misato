f25c0a867ba94ce8445d21060ae897dddb077921
George <georgej1088@gmail.com>
2020-08-03 22:17:34 -0400

    rework ban command, store bans in database
    no automatic unbanning yet, but the ban expiration dates are saved

--- bot/commands/moderation/ban.js
+++ bot/commands/moderation/ban.js
@@ -1,19 +1,64 @@
+const log = require('another-logger')({label: 'command:ban'});
 const {Command} = require('yuuko');
-const {parseGuildMember} = require('../../util/discord');
+const {parseGuildMember, parseTime, formatDateTime, awaitReaction} = require('../../util/discord');
 
-module.exports = new Command('ban', async (message, args) => {
-	const [member, reason] = parseGuildMember(args.join(' '), message.channel.guild);
+const confirmationEmoji = '✅';
+
+module.exports = new Command('ban', async (message, args, {db}) => {
+	const [member, rest] = parseGuildMember(args.join(' '), message.channel.guild);
 	if (!member) {
 		message.channel.createMessage('Not sure who you want me to ban. Start your message with a mention, exact tag, or user ID.').catch(() => {});
 		return;
 	}
-	// TODO: accept time, store bans in db, etc
-	try {
-		await member.ban(0, reason);
-		message.channel.createMessage('Banned!').catch(() => {});
-	} catch (_) {
-		message.channel.createMessage('Failed to ban. Are my permissions set up correctly?').catch(() => {});
+	const [duration, reason] = parseTime(rest);
+	const expirationDate = new Date(Date.now() + duration);
+	log.debug(member.id, duration, reason);
+
+	if (!reason || !duration && reason === rest) {
+		try {
+			const confirmation = await message.channel.createMessage(`Ban <@${member.id}> ${[duration ? '' : 'permanently', reason ? '' : 'without a reason'].filter(s => s).join(', ')}? React with ${confirmationEmoji} to confirm.`);
+			confirmation.addReaction(confirmationEmoji).catch(() => {});
+			await awaitReaction(confirmation, confirmationEmoji, message.author.id);
+		} catch (_) {
+			// couldn't send confirmation message or user aborted
+			return;
+		}
 	}
+
+	try {
+		// TODO: check if member is already banned before banning again
+		// TODO: service to clear bans after they expire
+
+		// Ban the user
+		await member.ban(0, reason);
+	} catch (_) {
+		message.channel.createMessage('Failed to ban. Are permissions set up correctly? I need the "Ban Members" permission, and I can\'t ban users with a higher role than me.').catch(() => {});
+		return;
+	}
+
+	// Create the ban record in the database
+	const banRecord = {
+		userID: member.id,
+		guildID: message.channel.guild.id,
+		modID: message.author.id,
+		date: new Date(),
+		note: reason,
+	};
+	// expirationDate is only stored if the duration is not zero
+	if (duration !== 0) {
+		banRecord.expirationDate = expirationDate;
+	}
+	try {
+		// Insert information to database
+		await db.collection('bans').insertOne(banRecord);
+	} catch (error) {
+		message.channel.createMessage(`Banned <@${member.id}>, but there was an error writing the ban to the database. Have a developer check the logs, this should not happen.`).catch(() => {});
+		log.error(error);
+		log.error('Rejected document:', banRecord);
+		return;
+	}
+
+	message.channel.createMessage(`Banned <@${member.id}> ${duration ? `until ${formatDateTime(expirationDate)}.` : 'permanently'}`).catch(() => {});
 }, {
 	permissions: [
 		'banMembers',
