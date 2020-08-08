04174a28b8f77e6634130fe2e60fcf813a0ab514
George <georgej1088@gmail.com>
2020-08-07 21:21:57 -0400

    More obvious UI for verifications

--- web/frontend/verify/App.vue
+++ web/frontend/verify/App.vue
@@ -1,42 +1,93 @@
 <template>
-	<div class="container app">
-		<div class="section">
-			<template v-if="!loaded">
-				<p>Loading...</p>
+	<div class="app">
+		<b-navbar
+			type="is-primary"
+			wrapper-class="container"
+		>
+			<template slot="brand">
+				<b-navbar-item tag="span">
+					/r/anime Discord!
+				</b-navbar-item>
 			</template>
-			<template v-else>
-				<template v-if="discordInfo">
-					<p>You're logged in as @{{ discordInfo.username }}#{{ discordInfo.discriminator }}. <a :href="`/auth/discord/logout?next=${temp}`">Log out</a></p>
-				</template>
-				<p v-else>
-					<a
-						:href="`/auth/discord?next=${temp}`"
-						class="button"
-					>Log in with Discord</a>
-				</p>
-				<template v-if="redditInfo">
-					<p>You're logged in as /u/{{ redditInfo.name }}. <a :href="`/auth/reddit/logout?next=${temp}`">Log out</a></p>
-				</template>
-				<p v-else>
-					<a
-						:href="`/auth/reddit?next=${temp}`"
-						class="button"
-					>Log in with Reddit</a>
-				</p>
-			</template>
-		</div>
-		<div class="section">
-			<p v-if="discordInfo && redditInfo">
-				<button
-					class="button"
-					@click="linkAccounts()"
+			<template slot="end">
+				<b-navbar-dropdown
+					v-if="discordInfo"
+					right
+					boxed
 				>
-					Link accounts
-				</button>
-			</p>
-			<p v-else>
-				Log in with both Reddit and Discord to link your accounts.
-			</p>
+					<template slot="label">
+						<figure class="avatar image is-32x32">
+							<img :src="discordInfo.avatarURL + '?size=64'">
+						</figure>
+					</template>
+					<b-navbar-item tag="span">
+						<span>Hi, <strong>{{ discordInfo.username }}#{{ discordInfo.discriminator }}</strong></span>
+					</b-navbar-item>
+					<b-navbar-item :href="'/auth/discord/logout?next=' + temp">
+						Log out
+					</b-navbar-item>
+				</b-navbar-dropdown>
+			</template>
+		</b-navbar>
+		<div class="container">
+			<div class="section">
+				<template v-if="!loaded">
+					<p>Loading...</p>
+				</template>
+				<template v-else>
+					<div class="level">
+						<div class="level-item">
+							<template v-if="discordInfo">
+								<p>
+									You're logged in as @{{ discordInfo.username }}#{{ discordInfo.discriminator }}.
+									<a
+										:href="`/auth/discord/logout?next=${temp}`"
+									>Log out</a>
+								</p>
+							</template>
+							<p v-else>
+								<a
+									:href="`/auth/discord?next=${temp}`"
+									class="button"
+								>Log in with Discord</a>
+							</p>
+						</div>
+						<div class="level-item">
+							<template v-if="redditInfo">
+								<p>
+									You're logged in as /u/{{ redditInfo.name }}.
+									<a
+										:href="`/auth/reddit/logout?next=${temp}`"
+									>Log out</a>
+								</p>
+							</template>
+							<p v-else>
+								<a
+									:href="`/auth/reddit?next=${temp}`"
+									class="button"
+								>Log in with Reddit</a>
+							</p>
+						</div>
+					</div>
+				</template>
+			</div>
+			<div class="section">
+				<div class="level">
+					<div class="level-item">
+						<p v-if="discordInfo && redditInfo">
+							<button
+								class="button is-large is-success"
+								@click="linkAccounts()"
+							>
+								Link these accounts
+							</button>
+						</p>
+						<p v-else>
+							Log in with both Reddit and Discord to link your accounts.
+						</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -74,9 +125,9 @@ export default {
 		linkAccounts () {
 			fetch(`/api/verification/${this.guildID}`, {method: 'POST'}).then(async response => {
 				if (response.ok) {
-					alert('Linked!');
+					alert('Accounts linked! You should now have access to the server. You can close this window now.');
 				} else {
-					alert('Failed to link');
+					alert('Failed to link your accounts. Get in touch with a chat moderator and give them your Reddit username.');
 				}
 			});
 		},
@@ -95,3 +146,11 @@ export default {
 </script>
 
 <style lang="scss" src="../common/customization.scss"></style>
+<style lang="scss">
+.avatar {
+  img {
+    border-radius: 50%;
+    max-height: none;
+  }
+}
+</style>
