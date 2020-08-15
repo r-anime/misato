e5253f4fd0f2d20a7b47a064a8985c8561548d32
George <georgej1088@gmail.com>
2020-08-15 13:37:26 -0400

    uhh more stuff

--- web/frontend/router.js
+++ web/frontend/router.js
@@ -5,11 +5,22 @@ Vue.use(VueRouter);
 
 import Home from './views/Home';
 import Verify from './views/Verify';
+import Management from './views/Management';
+
+import ManagementInfo from './views/management/Info';
 
 export default new VueRouter({
 	mode: 'history',
 	routes: [
 		{path: '/', component: Home, name: 'home'},
 		{path: '/verify/:guildID', component: Verify, name: 'verify'},
+		{
+			path: '/management/:guildID',
+			component: Management,
+			name: 'management',
+			children: [
+				{path: '', component: ManagementInfo, name: 'management-info'},
+			],
+		},
 	],
 });
