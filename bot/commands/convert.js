fe92c22f751e9b295eca022e94519e7d36eef770
bernasx <bernardo.ribeiro99@gmail.com>
2020-09-07 22:40:29 +0100

    Added convert command

---
+++ bot/commands/convert.js
@@ -0,0 +1,43 @@
+// Converts from one currency to another based on this API: http://exchangeratesapi.io/
+
+const {Command} = require('yuuko');
+const fetch = require('node-fetch');
+const convert = require('convert-units');
+// TODO: make it so it automatically uppercases every currency and lowercases the rest, also a list for codes
+function convertUnits (msg, baseValue, baseType, targetType) {
+	const conversion = convert(baseValue).from(baseType).to(targetType);
+	msg.channel.createMessage(`${baseValue}${baseType} is equal to ${conversion}${targetType}`);
+}
+
+async function convertCurrency (msg, baseValue, baseType, targetType, prefix) {
+	try {
+		const res = await fetch(`https://api.exchangeratesapi.io/latest?base=${baseType}&symbols=${targetType}`);
+		if (res.status !== 200) {
+			throw new Error(`Error fetching from API or you introduced the wrong units. Type ${prefix}currencyUnits to see every unit code.`);
+		}
+		const conversions = await res.json();
+		const result = conversions.rates[targetType] * baseValue;
+		msg.channel.createMessage(`${baseValue}${baseType} is roughly equal to ${result.toFixed(2)}${targetType}`);
+	} catch (err) {
+		msg.channel.createMessage(err.message).catch(() => {});
+	}
+}
+
+module.exports = new Command('convert', async (msg, args, context) => {
+	const baseValue = args[0].replace(/,/g, '.'); // replace commas with dots since some people use those for decimals
+	const baseType = args[1];
+	const targetType = args[2];
+	const prefix = context.prefix;
+	try {
+		// see if it can convert it normally first
+		convertUnits(msg, baseValue, baseType, targetType);
+	} catch (err) {
+		// if it can't convert it normally, try to find it as a currency, throws if it can't find a currency either
+		convertCurrency(msg, baseValue, baseType, targetType, prefix);
+	}
+});
+
+module.exports.help = {
+	args: '',
+	desc: 'Converts from one currency to another based on this API: http://exchangeratesapi.io/',
+};
