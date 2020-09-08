174f33520d13dc2b6a61e03eef2c4e8b637821cd
bernasx <bernardo.ribeiro99@gmail.com>
2020-09-08 18:07:33 +0100

    Added some QoL help to finding units and currency codes

--- bot/commands/convert.js
+++ bot/commands/convert.js
@@ -3,6 +3,7 @@
 const {Command} = require('yuuko');
 const fetch = require('node-fetch');
 const convert = require('convert-units');
+
 function convertUnits (msg, baseValue, baseType, targetType) {
 	const conversion = convert(baseValue).from(baseType).to(targetType);
 	msg.channel.createMessage(`${baseValue}${baseType} is equal to ${conversion}${targetType}`);
@@ -14,7 +15,7 @@ async function convertCurrency (msg, baseValue, baseType, targetType, prefix) {
 	try {
 		const res = await fetch(`https://api.exchangeratesapi.io/latest?base=${baseType}&symbols=${targetType}`);
 		if (res.status !== 200) {
-			throw new Error(`Error fetching from API or you introduced the wrong units. Type ${prefix}currencyUnits to see every unit code.`);
+			throw new Error(`Error fetching from API or you introduced the wrong units. Type \`${prefix}convert\` to see every unit code.`);
 		}
 		const conversions = await res.json();
 		const result = conversions.rates[targetType] * baseValue;
@@ -24,7 +25,14 @@ async function convertCurrency (msg, baseValue, baseType, targetType, prefix) {
 	}
 }
 
-module.exports = new Command('convert', async (msg, args, context) => {
+module.exports = new Command('convert', (msg, args, context) => {
+	if (args.length === 0) {
+		msg.channel.createMessage(`For currency codes visit: <https://www.xe.com/en/iso4217.php>
+		\rFor units visit: <https://www.npmjs.com/package/convert-units>
+		\rCommand can be used with: \`${context.prefix}convert [amount] [baseUnit] [targetUnit]\``);
+		return;
+	}
+
 	const baseValue = args[0].replace(/,/g, '.'); // replace commas with dots since some people use those for decimals
 	const baseType = args[1];
 	const targetType = args[2];
@@ -37,8 +45,8 @@ module.exports = new Command('convert', async (msg, args, context) => {
 		convertCurrency(msg, baseValue, baseType, targetType, prefix);
 	}
 });
-
+// TODO: add args
 module.exports.help = {
 	args: '',
-	desc: 'Converts from one currency to another based on this API: http://exchangeratesapi.io/',
+	desc: 'Converts units and currency, the latter being based on this API: <http://exchangeratesapi.io/>',
 };
