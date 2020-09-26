e89af447e466f1687d4c47505c255af3198c83f4
Geo <georgej1088@gmail.com>
2020-09-26 15:02:23 -0400

    Fix avatar command for bernas (#52)

--- bot/commands/misc/avatar.js
+++ bot/commands/misc/avatar.js
@@ -4,7 +4,10 @@ const {Command} = require('yuuko');
 const {parseUser} = require('../../util/discord');
 
 module.exports = new Command('avatar', async (msg, args) => {
-	if (args.length === 0) msg.channel.createMessage(msg.author.avatarURL).catch(() => {});
+	if (args.length === 0) {
+		msg.channel.createMessage(msg.author.avatarURL).catch(() => {});
+		return;
+	}
 	const [member] = await parseUser(args.join(' '), msg.channel.guild, msg.author);
 	if (!member) {
 		msg.channel.createMessage('Member not found, check if what you\'re using is correct or try using an ID.').catch(() => {});
