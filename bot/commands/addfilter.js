5bbcf89797b03ffe10bcce3889da8e8ec28a9479
Geo <georgej1088@gmail.com>
2020-10-07 00:04:49 -0400

    disable addfilter + add permission check

--- bot/commands/addfilter.js
+++ bot/commands/addfilter.js
@@ -2,16 +2,17 @@ const {Command} = require('yuuko');
 const {code} = require('../util/formatting');
 
 module.exports = new Command('addfilter', async (message, args, {db}) => {
-	const filter = {
-		guildID: message.guildID,
-		rule: {
-			type: 'containsText',
-			field: 'content',
-			text: args.join(' '),
-		},
-	};
-	await db.collection('messageFilters').insertOne(filter);
-	message.channel.createMessage(`Created new filter for that.${code(JSON.stringify(filter, null, 2), 'json')}`);
+	// const filter = {
+	// 	guildID: message.guildID,
+	// 	rule: {
+	// 		type: 'containsText',
+	// 		field: 'content',
+	// 		text: args.join(' '),
+	// 	},
+	// };
+	// await db.collection('messageFilters').insertOne(filter);
+	// message.channel.createMessage(`Created new filter for that.${code(JSON.stringify(filter, null, 2), 'json')}`);
 }, {
 	guildOnly: true,
+	permissions: ['manageMessages'],
 });
