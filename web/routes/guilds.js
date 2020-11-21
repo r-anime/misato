6a6e9b3e2ce02311d4ae436ab98f511580b4909b
George <georgej1088@gmail.com>
2020-11-21 12:36:05 -0500

    very rough draft of API for notes/warnings/etc

--- web/routes/guilds.js
+++ web/routes/guilds.js
@@ -12,5 +12,86 @@ module.exports = (db, client) => polka()
 			response.writeHead(404);
 			response.end();
 		}
-	});
+	})
 
+	// TODO: auth
+	// NOMERGE
+	.get('/:guildID/notes', async (request, response) => {
+		const {guildID} = request.params;
+		try {
+			response.end(JSON.stringify(await db.collection('notes').find({guildID}).toArray()));
+		} catch (error) {
+			response.writeHead(500);
+			response.end();
+		}
+	})
+
+	.get('/:guildID/members/:userID/notes', async (request, response) => {
+		const {guildID, userID} = request.params;
+		try {
+			response.end(JSON.stringify(await db.collection('notes').find({guildID, userID}).toArray()));
+		} catch (error) {
+			response.writeHead(500);
+			response.end();
+		}
+	})
+
+	.get('/:guildID/warnings', async (request, response) => {
+		const {guildID} = request.params;
+		try {
+			response.end(JSON.stringify(await db.collection('warnings').find({guildID}).toArray()));
+		} catch (error) {
+			response.writeHead(500);
+			response.end();
+		}
+	})
+
+	.get('/:guildID/members/:userID/warnings', async (request, response) => {
+		const {guildID, userID} = request.params;
+		try {
+			response.end(JSON.stringify(await db.collection('warnings').find({guildID, userID}).toArray()));
+		} catch (error) {
+			response.writeHead(500);
+			response.end();
+		}
+	})
+
+	.get('/:guildID/kicks', async (request, response) => {
+		const {guildID} = request.params;
+		try {
+			response.end(JSON.stringify(await db.collection('kicks').find({guildID}).toArray()));
+		} catch (error) {
+			response.writeHead(500);
+			response.end();
+		}
+	})
+
+	.get('/:guildID/members/:userID/kicks', async (request, response) => {
+		const {guildID, userID} = request.params;
+		try {
+			response.end(JSON.stringify(await db.collection('kicks').find({guildID, userID}).toArray()));
+		} catch (error) {
+			response.writeHead(500);
+			response.end();
+		}
+	})
+
+	.get('/:guildID/bans', async (request, response) => {
+		const {guildID} = request.params;
+		try {
+			response.end(JSON.stringify(await db.collection('bans').find({guildID}).toArray()));
+		} catch (error) {
+			response.writeHead(500);
+			response.end();
+		}
+	})
+
+	.get('/:guildID/members/:userID/bans', async (request, response) => {
+		const {guildID, userID} = request.params;
+		try {
+			response.end(JSON.stringify(await db.collection('bans').find({guildID, userID}).toArray()));
+		} catch (error) {
+			response.writeHead(500);
+			response.end();
+		}
+	});
