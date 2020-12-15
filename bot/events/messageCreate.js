0080095563b0a7e1bb0a17459d7593495feef874
George <georgej1088@gmail.com>
2020-12-15 06:13:12 -0500

    *actually* fix filters, dont drop every message

--- bot/events/messageCreate.js
+++ bot/events/messageCreate.js
@@ -7,19 +7,20 @@ module.exports = new EventListener('messageCreate', async (message, {client, db}
 	if (message.guildID) {
 		// Fetch filter for this guild
 		const configuration = await db.collection('messageFilters').findOne({guildID: message.guildID});
-		if (!configuration || !configuration.rule) return;
-		const {rule} = configuration;
+		if (configuration && configuration.rule) {
+			const {rule} = configuration;
 
-		// Map each filter to a promise returning whether or not the message matches that filter
-		if (!isValidRule(rule)) {
-			log.error('Invalid rulE???', rule);
-			return false;
-		}
+			// Map each filter to a promise returning whether or not the message matches that filter
+			if (!isValidRule(rule)) {
+				log.error('Invalid rulE???', rule);
+				return false;
+			}
 
-		// As soon as any of those promises resolves to true, we know the message is invalid, so we delete it
-		if (await messageMatchesRule(message, rule)) {
-			message.delete().catch(() => {});
-			return;
+			// As soon as any of those promises resolves to true, we know the message is invalid, so we delete it
+			if (await messageMatchesRule(message, rule)) {
+				message.delete().catch(() => {});
+				return;
+			}
 		}
 	}
 
