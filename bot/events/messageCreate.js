9515273dbc41d9c92966850f73282042551af2ee
Geo <georgej1088@gmail.com>
2021-03-08 21:56:51 -0500

    Filter functions in commands (#101)
    * .filteradd command
    
    * fix handling of invalid filter rules while im here
    
    * don't filter people with manage messages
    
    * insert filters in lower case
    
    * filterdel command
    
    * add no args messages to filteradd/filterdel

--- bot/events/messageCreate.js
+++ bot/events/messageCreate.js
@@ -7,19 +7,20 @@ module.exports = new EventListener('messageCreate', async (message, {client, db}
 	if (message.guildID) {
 		// Fetch filter for this guild
 		const configuration = await db.collection('messageFilters').findOne({guildID: message.guildID});
-		if (configuration && configuration.rule) {
-			const {rule} = configuration;
 
-			// Map each filter to a promise returning whether or not the message matches that filter
-			if (!isValidRule(rule)) {
-				log.error('Invalid rulE???', rule);
-				return false;
-			}
-
-			// As soon as any of those promises resolves to true, we know the message is invalid, so we delete it
-			if (await messageMatchesRule(message, rule)) {
-				message.delete().catch(() => {});
-				return;
+		// TODO: temporary hardcode to ignore the filter for people with manage messages
+		if (!message.channel.permissionsOf(message.author.id).has('manageMessages')) {
+			if (configuration && configuration.rule) {
+				const {rule} = configuration;
+				if (isValidRule(rule)) {
+					if (await messageMatchesRule(message, rule)) {
+						message.delete().catch(() => {});
+						return;
+					}
+				} else {
+					// weird
+					log.error('Encountered invalid rule in guild', message.guildID, rule);
+				}
 			}
 		}
 	}
