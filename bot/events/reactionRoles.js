4cc31f3b3a0b5791be93a1c72da3776ccd6461eb
Erin <erin20913@gmail.com>
2021-04-02 21:12:49 -0400

    also handle reaction removals

--- bot/events/reactionRoles.js
+++ bot/events/reactionRoles.js
@@ -4,6 +4,20 @@ const log = require('another-logger')({label: 'event:reactionRoles'});
 // A list of all the current reaction role configurations
 let reactionRoles = [];
 
+// Returns the reaction config for the given reaction information, if any
+function findReactConfig (message, emoji) {
+	return reactionRoles.find(rr => {
+		// Snowflakes are unique, so if message ID matches we can assume channel and guild ID also match.
+		if (message.id !== rr.messageID) return false;
+
+		// For custom emojis, compare the emoji ID. For native emojis, the ID will be unset and the "name" will be
+		// the unicode string.
+		if (rr.emoji !== (emoji.id || emoji.name)) return false;
+
+		return true;
+	});
+}
+
 module.exports = new EventListener('ready', ({client, db}) => {
 	// Refresh the list of reaction roles every 30 seconds
 	async function fetchReactionRoles () {
@@ -24,16 +38,7 @@ module.exports = new EventListener('ready', ({client, db}) => {
 		}
 
 		// Find the rule we care about for this reaction
-		const relevantReactionRoleConfig = reactionRoles.find(rr => {
-			// Snowflakes are unique, so if message ID matches we can assume channel and guild ID also match.
-			if (message.id !== rr.messageID) return false;
-
-			// For custom emojis, compare the emoji ID. For native emojis, the ID will be unset and the "name" will be
-			// the unicode string.
-			if (rr.emoji !== (emoji.id || emoji.name)) return false;
-
-			return true;
-		});
+		const relevantReactionRoleConfig = findReactConfig(message, emoji);
 
 		// If this reaction doesn't match a rule, do nothing with it
 		if (!relevantReactionRoleConfig) return;
@@ -46,6 +51,27 @@ module.exports = new EventListener('ready', ({client, db}) => {
 		} catch (error) {
 			// probably don't have permission, or the user is above us in the role list
 			log.error(`Error adding role ${relevantReactionRoleConfig.roleID} to member ${userID}:`, error);
+		}
+	});
+
+	// Handle removed reactions
+	client.on('messageReactionRemove', async (message, emoji, userID) => {
+		// If we're not in a guild, we know we can't have reaction roles
+		if (!message.channel.guild) {
+			return;
+		}
+
+		// Find the rule we care about for this reaction
+		const relevantReactionRoleConfig = findReactConfig(message, emoji);
+
+		// If this reaction doesn't match a rule, do nothing with it
+		if (!relevantReactionRoleConfig) return;
+
+		// Remove the role from the user
+		try {
+			await message.channel.guild.removeMemberRole(userID, relevantReactionRoleConfig.roleID);
+		} catch (error) {
+			log.error(`Error removing role ${relevantReactionRoleConfig.roleID} from member ${userID}:`, error);
 		}
 	});
 }, {
