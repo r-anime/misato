d1388d51a8d55ee176ffc5939117c61411b690ee
Erin <erin20913@gmail.com>
2021-05-04 15:40:13 -0400

    Support keeping channels in store too

--- web/frontend/store.js
+++ web/frontend/store.js
@@ -6,11 +6,13 @@ Vue.use(Vuex);
 // Promises used to track whether or not something is already being fetched
 let fetchDiscordInfoPromise = null;
 let fetchGuildRolesPromise = null;
+let fetchGuildChannelsPromise = null;
 
 export default new Vuex.Store({
 	state: {
 		discordInfo: undefined,
 		guildRoles: new Map(),
+		guildChannels: new Map(),
 	},
 	mutations: {
 		SET_DISCORD_INFO (state, info) {
@@ -18,6 +20,9 @@ export default new Vuex.Store({
 		},
 		SET_GUILD_ROLES (state, {guildID, roles}) {
 			state.guildRoles.set(guildID, roles);
+		},
+		SET_GUILD_CHANNELS (state, {guildID, channels}) {
+			state.guildChannels.set(guildID, channels);
 		},
 	},
 	actions: {
@@ -41,6 +46,17 @@ export default new Vuex.Store({
 				});
 			}
 			return fetchGuildRolesPromise;
+		},
+		fetchGuildChannels ({commit}, guildID) {
+			if (!fetchGuildChannelsPromise) {
+				fetchGuildChannelsPromise = fetch(`/api/guilds/${guildID}/channels`).then(async response => {
+					commit('SET_GUILD_CHANNELS', {
+						guildID,
+						channels: response.ok ? await response.json().catch(() => null) : null,
+					});
+					fetchGuildRolesPromise = null;
+				});
+			}
 		},
 	},
 });
