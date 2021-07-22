811719c1f99e9b76e2564b93ff274011a8d615d9
Erin <erin20913@gmail.com>
2021-07-22 11:39:41 -0400

    Add autoRevokeDate to bans; add descriptions

---
+++ migrations/1626966666740-add-unbannedAt-to-bans.js
@@ -0,0 +1,91 @@
+const {MongoClient} = require('mongodb');
+const config = require('../config');
+
+module.exports.up = async () => {
+	const mongoClient = new MongoClient(config.mongodb.url, {useUnifiedTopology: true});
+	await mongoClient.connect();
+	const db = mongoClient.db(config.mongodb.databaseName);
+
+	await db.command({
+		collMod: 'bans',
+		validator: {
+			$jsonSchema: {
+				bsonType: 'object',
+				required: ['userID', 'guildID', 'date', 'note'],
+				properties: {
+					userID: {
+						bsonType: 'string',
+						description: 'The ID of the user the ban applies to',
+					},
+					guildID: {
+						bsonType: 'string',
+						description: 'The ID of the guild the ban applies to',
+					},
+					modID: {
+						bsonType: 'string',
+						description: 'The ID of the user who created the ban',
+					},
+					date: {
+						bsonType: 'date',
+						description: 'The date the ban was created',
+					},
+					expirationDate: {
+						bsonType: 'date',
+						description: 'The date the ban should automatically expire, if any',
+					},
+					revokeDate: {
+						bsonType: 'date',
+						description: 'The date the ban was manually revoked through the unban command',
+					},
+					autoRevokeDate: {
+						bsonType: 'date',
+						description: 'The date the ban was automatically revoked by the expiration date passing',
+					},
+					note: {
+						bsonType: 'string',
+						description: 'The text of the notification sent to the user',
+					},
+				},
+			},
+		},
+	});
+};
+
+module.exports.down = async () => {
+	const mongoClient = new MongoClient(config.mongodb.url, {useUnifiedTopology: true});
+	await mongoClient.connect();
+	const db = mongoClient.db(config.mongodb.databaseName);
+
+	await db.command({
+		collMod: 'bans',
+		validator: {
+			$jsonSchema: {
+				bsonType: 'object',
+				required: ['userID', 'guildID', 'date', 'note'],
+				properties: {
+					userID: {
+						bsonType: 'string',
+					},
+					guildID: {
+						bsonType: 'string',
+					},
+					modID: {
+						bsonType: 'string',
+					},
+					date: {
+						bsonType: 'date',
+					},
+					expirationDate: {
+						bsonType: 'date',
+					},
+					revokeDate: {
+						bsonType: 'date',
+					},
+					note: {
+						bsonType: 'string',
+					},
+				},
+			},
+		},
+	});
+};
