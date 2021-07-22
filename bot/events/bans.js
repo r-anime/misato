d6bd2b2b9aa1bce8c67b5184ff0dfbd3543b31df
Erin <erin20913@gmail.com>
2021-07-22 11:57:09 -0400

    Don't delete temporary bans, log more errors

--- bot/events/bans.js
+++ bot/events/bans.js
@@ -10,14 +10,35 @@ module.exports = new EventListener('ready', ({client, db}) => {
 	// TODO: same crap as with reminders
 	async function checkBans () {
 		// Fetch bans from the database that have become due since the last check
-		const expiredBans = await collection.find({expirationDate: {$lt: new Date()}}).toArray();
+		const expiredBans = await collection.find({
+			expirationDate: {$lt: new Date()},
+			revokeDate: {$exists: false},
+			autoRevokeDate: {$exists: false},
+		}).toArray();
 		log.debug('Expired bans:', expiredBans);
 
 		expiredBans.forEach(ban => {
 			// Try to unban the guild member. If it fails, either the ban no longer exists or we don't have permissions
-			client.unbanGuildMember(ban.guildID, ban.userID).catch(() => {});
-			// Remove the ban from the database, log on error
-			collection.deleteOne({_id: ban._id}).catch(log.error);
+			client.unbanGuildMember(ban.guildID, ban.userID).catch(error => {
+				if (error.code === 10026) { // Unknown Ban
+					// This isn't an error condition, the ban was just manually
+					// revoked by a mod without using the unban command (or we
+					// unbanned the user last run but the info wasn't updated).
+					// We'll treat it as if we'd just unbanned the user anyway.
+					return;
+				}
+				throw error;
+			}).then(async () => {
+				// Update the ban in the database
+				await collection.updateOne({_id: ban._id}, {
+					$set: {
+						autoRevokeDate: new Date(),
+					},
+				});
+			}).catch(error => {
+				// Unbanning the user failed, or updating the ban info failed.
+				log.error(`Failed to unban guild member ${ban.userID} or update ban in database:`, error);
+			});
 		});
 	}
 	setInterval(() => {
@@ -26,7 +47,7 @@ module.exports = new EventListener('ready', ({client, db}) => {
 		} else {
 			log.warn('Client disconnected while trying to check bans; skipping');
 		}
-	}, 60 * 1000);
+	}, 1000);
 }, {
 	once: true,
 });
