13cbb721e9ea73bb0d8e379dbfb1dc2018613509
Erin <erin20913@gmail.com>
2021-07-23 15:28:55 -0400

    Fix the other instance of this issue

--- src/bot/util/formatting.js
+++ src/bot/util/formatting.js
@@ -1,36 +1,33 @@
 // Provides helper functions for formatting Discord messages.
 
-export default {
+/**
+ * Wraps the given text in a blockquote by adding `>` to the beginning of
+ * each line of the text.
+ * @param {string} text
+ * @returns {string}
+ */
+export function	blockquote (text) {
+	return text.replace(/^|\n(?!$)/g, '$&> ');
+}
 
-	/**
-	 * Wraps the given text in a blockquote by adding `>` to the beginning of
-	 * each line of the text.
-	 * @param {string} text
-	 * @returns {string}
-	 */
-	blockquote (text) {
-		return text.replace(/^|\n(?!$)/g, '$&> ');
-	},
+/**
+ * Wraps the given text in a code block of the given language. Inserts
+ * zero-width spaces into existing sequences of triple backticks to avoid
+ * malicious formatting, unless the unsafe argument is set to true.
+ * @param {string} text
+ * @param {string} [lang='']
+ * @param {boolean} [unsafe=false]
+ * @returns {string}
+ */
+export function	code (text, lang = '', unsafe = false) {
+	return `\`\`\`${lang ? `${lang}\n` : ''}${unsafe ? text : text.replace(/```/g, '`​`​`')}\`\`\``;
+}
 
-	/**
-	 * Wraps the given text in a code block of the given language. Inserts
-	 * zero-width spaces into existing sequences of triple backticks to avoid
-	 * malicious formatting, unless the unsafe argument is set to true.
-	 * @param {string} text
-	 * @param {string} [lang='']
-	 * @param {boolean} [unsafe=false]
-	 * @returns {string}
-	 */
-	code (text, lang = '', unsafe = false) {
-		return `\`\`\`${lang ? `${lang}\n` : ''}${unsafe ? text : text.replace(/```/g, '`​`​`')}\`\`\``;
-	},
-
-	/**
-	 * Escapes a string for display without formatting.
-	 * @param {string} text
-	 * @returns {string}
-	 */
-	escape (text) {
-		return text.replace(/[*_`~]|(\n|^)>/g, '\\$&');
-	},
-};
+/**
+ * Escapes a string for display without formatting.
+ * @param {string} text
+ * @returns {string}
+ */
+export function	escape (text) {
+	return text.replace(/[*_`~]|(\n|^)>/g, '\\$&');
+}
