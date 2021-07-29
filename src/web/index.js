db6ac830d80dddf3c82f7b1c8089a6a525f37710
Erin <erin20913@gmail.com>
2021-07-29 17:09:23 -0400

    Remove static file server

--- src/web/index.js
+++ src/web/index.js
@@ -11,13 +11,12 @@ import responseHelpers from './middleware/responseHelpers';
 import logging from './middleware/logging';
 import auth from './routes/auth';
 import api from './routes/api';
-import sirv from 'sirv';
 
 export default (mongoClient, db, discordClient) => {
 	// Set up our app
 	const app = polka();
 
-	// Set up session storage, delegated to parent process via IPC
+	// Set up our middlewares
 	app.use(session({
 		store: new MongoStore({
 			client: mongoClient,
@@ -27,20 +26,7 @@ export default (mongoClient, db, discordClient) => {
 		saveUninitialized: false,
 		resave: false,
 	}));
-
 	app.use(logging);
-
-	// Set up static serving of built frontend bundles
-	app.use(sirv(config.web.frontendBuildDir, {
-		dev: config.dev,
-		single: true,
-		ignores: [
-			'/api',
-			'/auth',
-		],
-	}));
-
-	// Set up our other middlewares
 	app.use(responseHelpers);
 
 	// Register sub-apps for API routes
