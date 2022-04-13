7c359f53045b507ef48d2ee057284d4b783e9d1a
mushroom <123withnumber@gmail.com>
2022-04-13 14:28:35 +0530

    Add whether a user is still a member or not to .whois output

--- src/bot/commands/whois.js
+++ src/bot/commands/whois.js
@@ -1,3 +1,5 @@
+import createLogger from 'another-logger';
+const log = createLogger({label: 'command:whois'});
 import {Command} from 'yuuko';
 import config from '../../../config';
 import {parseUser, formatDate} from '../util/discord';
@@ -69,6 +71,24 @@ async function notesLine (userID, guildID, db) {
 	}`;
 }
 
+/**
+ *Returns a string stating whether a user is still a member or not
+ * @async
+ * @param {Eris.Guild} guild
+ * @param {number} userID
+ * @returns {Promise<string>}
+ */
+async function isUserStillMember (guild, userID) {
+	let isMember;
+	try {
+		isMember = guild.members.get(userID) && await guild.getRESTMember(userID) ? 'Yes' : 'No';
+	} catch (error) {
+		log.error(`Error while getting member ${userID} from guild ${guild.id}`, error);
+		isMember = 'N/A';
+	}
+	return `__Still Member? **${isMember}**__`;
+}
+
 const command = new Command('whois', async (message, args, context) => {
 	if (!args.length) {
 		return context.sendHelp(message, context);
@@ -98,6 +118,7 @@ const command = new Command('whois', async (message, args, context) => {
 		`<${config.web.host}/guilds/${message.channel.guild.id}/members/${user.id}>`,
 		`__Username: **${user.username}#${user.discriminator}**__`,
 		`__Account Age: **${formatDate(new Date(user.createdAt))}**__`,
+		isUserStillMember(message.channel.guild, user.id),
 		redditLine(user.id, message.channel.guild.id, db),
 		warningsLine(user.id, message.channel.guild.id, db),
 		kicksLine(user.id, message.channel.guild.id, db),
