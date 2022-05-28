a4f85f3f67337e96f29aa0659115fefc765b9713
mushroom <123withnumber@gmail.com>
2022-05-28 10:00:21 +0530

    Create new file for messageDeleteEvent

---
+++ src/bot/events/messageDelete.js
@@ -0,0 +1,41 @@
+import Eris from 'eris';
+import {EventListener} from 'yuuko';
+import config from '../../../config';
+import {blockquote} from '../util/formatting';
+
+//  Should consider adding whether it was deleted by a mod or the user by cross referencing audit logs
+
+export default new EventListener('messageDelete', async (message, {client}) => {
+	setTimeout(async () => {
+		// Ignore if the message was deleted by a bot
+	let logs = await message.channel.guild.getAuditLogs(50, null, 72);
+	// Check if message was deleted in a DM or other guilds and do not log if so
+	if (!message.guildID || message.guildID !== config.TEMP_guildID) {
+		return;
+	}
+
+	// Check if the message is a partial message and if so do not log
+	if (!(message instanceof Eris.Message)) {
+		return;
+	}
+	client.createMessage(config.TEMP_loggingChannelID, {
+		embed: {
+			title: 'Message Deleted',
+			fields: [
+				{
+					name: 'Channel',
+					value: `<#${message.channel.id}>\n\`${message.channel.id}\``,
+					inline: true,
+				},
+				{
+					name: 'Author',
+					value: `<@${message.author.id}>\n\`${message.author.id}\``,
+					inline: true,
+				},
+			],
+			description: blockquote(message.content),
+			timestamp: new Date(message.createdAt),
+		},
+	}).catch(() => {});
+	}, 2000);
+});
