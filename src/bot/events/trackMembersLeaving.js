877206238ab5eb7a23b0ee505e88da776dfb3ab1
mushroom-disaster <102722190+mushroom-disaster@users.noreply.github.com>
2022-06-08 20:00:22 +0530

    Make bot log when a user stops boosting and when they leave (#197)

---
+++ src/bot/events/trackMembersLeaving.js
@@ -0,0 +1,24 @@
+import {EventListener} from 'yuuko';
+import config from '../../../config';
+
+export default new EventListener('guildMemberRemove', async (guild, member, {client}) => {
+	if (member.bot) return;
+	if (guild.id !== config.TEMP_guildID) return;
+
+	if (member.premiumSince !== null) {
+		await client.createMessage(config.TEMP_loggingChannelID, {
+			content: `User **<@${member.id}> (${member.username}#${member.discriminator})** has stopped boosting`,
+			allowedMentions: {
+				users: false,
+			},
+		}).catch(() => {});
+	}
+
+
+	await client.createMessage(config.TEMP_loggingChannelID, {
+		content: `User **<@${member.id}> (${member.username}#${member.discriminator})** has left the server`,
+		allowedMentions: {
+			users: false,
+		},
+	}).catch(() => {});
+});
