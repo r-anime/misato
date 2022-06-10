4de033e76ab0c030043e52d139667f784b427de0
MajehOS <32561001+MajehOS@users.noreply.github.com>
2022-06-11 02:34:10 +1000

    Switched dates back around

--- src/bot/commands/whois.js
+++ src/bot/commands/whois.js
@@ -67,7 +67,7 @@ async function bansLine (userID, guildID, db) {
 		}).toArray();
 
 		return `__Bans: **${numResults}**__${
-			results.map(ban => `\n- ${escape(formatDate(ban.date))}${ban.note && ban.expirationDate ? `: **\`${Math.ceil(Math.abs(ban.date - ban.expirationDate) / (1000 * 60 * 60 * 24))}d\` |** ${escape(ban.note)}` : ''} - <@${ban.modID}>`).join('')
+			results.map(ban => `\n- ${escape(formatDate(ban.date))}${ban.note && ban.expirationDate ? `: **\`${Math.ceil((ban.expirationDate - ban.date) / (1000 * 60 * 60 * 24))}d\` |** ${escape(ban.note)}` : ''} - <@${ban.modID}>`).join('')
 		}${
 			numResults > results.length ? '\nSee more on the website. (soon:tm:)' : ''
 		}`;
