b65a4a5a5ac592aff76050a8b9b298a7cec37c44
mushroom <123withnumber@gmail.com>
2022-06-12 22:01:52 +0530

    Remove await from createMessage in verification.js

--- src/bot/events/verification.js
+++ src/bot/events/verification.js
@@ -10,7 +10,7 @@ export default new EventListener('guildMemberAdd', async (guild, member, {db, cl
 	if (member.bot) return;
 	if (guild.id !== config.TEMP_guildID) return;
 
-	await client.createMessage(config.TEMP_joiningChannelID, {
+	client.createMessage(config.TEMP_joiningChannelID, {
 		content: `User **<@${member.id}> (${member.username}#${member.discriminator})** has joined 
 		\nAccount Created:  ${formatDate(new Date(member.createdAt))} (${formatDateRelative(new Date(member.createdAt))})`,
 		allowedMentions: {
