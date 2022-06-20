35fca8fe16764693fc5f37b71d9fa1e0254b138a
Erin <github@eritbh.me>
2022-06-20 18:56:03 -0400

    Dynamically fetch repo URL; add warning logs

--- src/bot/commands/ping.js
+++ src/bot/commands/ping.js
@@ -1,21 +1,40 @@
 // Tests the bot's response time.
 // Shows the current commit hash and repo URL
 
+import createLogger from 'another-logger';
 import {Command} from 'yuuko';
 import childProcess from 'child_process';
 
-let commitHash;
+const log = createLogger({label: 'cmd:ping'});
+
+let commitHash = null;
 childProcess.exec('git rev-parse --short HEAD', (err, stdout) => {
 	if (err) {
-		commitHash = 'Unknown';
 		return;
 	}
 	commitHash = stdout.trim();
 });
 
+let gitRepoURI = null;
+childProcess.exec('git remote get-url origin', (err, stdout) => {
+	if (err) {
+		log.warn('Failed to fetch git remote URI:', err);
+		return;
+	}
+	gitRepoURI = stdout.trim().replace(/\.git$/, '');
+});
+
 const command = new Command('ping', async msg => {
+	let messageContent = 'Pong!';
+	if (commitHash != null) {
+		messageContent += `\nCommit: ${commitHash}`;
+	}
+	if (gitRepoURI != null) {
+		messageContent += `\nRepo: <${gitRepoURI}>`;
+	}
+
 	const then = Date.now();
-	const newMsg = await msg.channel.createMessage(`Pong!\nCommit: ${commitHash}\nRepo: <https://github.com/r-anime/discord-mod-bot>`);
+	const newMsg = await msg.channel.createMessage(messageContent);
 	newMsg.edit(newMsg.content.replace('Pong!', `Pong! (${Date.now() - then}ms)`));
 });
 command.help = {
