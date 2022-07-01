f7d2a8f8fd079038133ad9d252e7eba8b57117ca
Erin <github@eritbh.me>
2022-07-01 13:48:53 -0400

    Create modal-test command

---
+++ src/bot/applicationCommands/test.ts
@@ -0,0 +1,51 @@
+import {SlashCommand} from "yuuko"
+import {Constants} from "eris"
+import {promptModal} from "../util/discord";
+import {blockquote, escape} from "../util/formatting";
+
+import createLogger from 'another-logger';
+const log = createLogger({label: 'cmd:modal-test'});
+
+export default new SlashCommand('modal-test', {
+	description: 'Pulls up a modal',
+}, async (interaction, {client}) => {
+	const modalInteraction = await promptModal(client, interaction, {
+		title: 'Hello there!',
+		components: [
+			{
+				type: Constants.ComponentTypes.ACTION_ROW,
+				components: [
+					{
+						custom_id: 'title',
+						label: 'Title',
+						type: Constants.ComponentTypes.TEXT_INPUT,
+						style: Constants.TextInputStyles.SHORT,
+						max_length: 200, // TODO: pick something better
+						required: true,
+					},
+				],
+			},
+			{
+				type: Constants.ComponentTypes.ACTION_ROW,
+				components: [
+					{
+						custom_id: 'message',
+						label: 'Message',
+						type: Constants.ComponentTypes.TEXT_INPUT,
+						style: Constants.TextInputStyles.PARAGRAPH,
+						max_length: 4000, // TODO: pick something better
+						required: true,
+					},
+				],
+			},
+		],
+	}, {
+		timeout: 10 * 30 * 1000,
+	});
+	modalInteraction.acknowledge();
+
+	interaction.createFollowup({
+		flags: Constants.MessageFlags.EPHEMERAL,
+		content: `Thanks for submitting **"${escape(modalInteraction.data.components[0].components[0].value)}"**\n${blockquote(escape(modalInteraction.data.components[1].components[0].value))}`,
+	});
+});
