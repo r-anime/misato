77e9d1cf49473c705250f40769699584f4c1d5a4
mushroom <123withnumber@gmail.com>
2022-08-04 20:50:03 +0530

    Added code for removing links

--- src/bot/events/messageDelete.js
+++ src/bot/events/messageDelete.js
@@ -8,14 +8,23 @@ import {blockquote} from '../util/formatting';
 export default new EventListener('messageDelete', (message, {client}) => {
 	setTimeout(() => {
 		// Check if message was deleted in a DM or other guilds and do not log if so
-		if (!message.guildID || message.guildID !== config.TEMP_guildID) {
-			return;
-		}
+		if (!message.guildID || message.guildID !== config.TEMP_guildID) return;
 
 		// Check if the message is a partial message and if so do not log
-		if (!(message instanceof Eris.Message)) {
-			return;
-		}
+		if (!(message instanceof Eris.Message)) return;
+
+		// Check if the message was from a bot
+		if (message.author.bot) return;
+
+		// Ignore if message author has manage message ie. is a mod
+		if (message.channel.permissionsOf(message.author.id).has('manageMessages')) return;
+
+		const urlRegex = /[(http(s)?)://(www.)?a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/;
+		const content = message.content.trim().replace(urlRegex, '');
+
+		// Ignore if message has no text content ie. message with just an attachment or just a link or both
+		if (content.length === 0) return;
+
 		client.createMessage(config.TEMP_loggingChannelID, {
 			embed: {
 				title: 'Message Deleted',
@@ -31,7 +40,7 @@ export default new EventListener('messageDelete', (message, {client}) => {
 						inline: true,
 					},
 				],
-				description: blockquote(message.content),
+				description: blockquote(content),
 				timestamp: new Date(message.createdAt),
 			},
 		}).catch(() => {});
