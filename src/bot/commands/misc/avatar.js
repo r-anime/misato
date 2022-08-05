f5624f707312ae3cee0425eb44d9afaf4ab75c09
mushroom <123withnumber@gmail.com>
2022-08-05 21:13:11 +0530

    Change avatar command to show server avatar, Add command for default avatar

--- src/bot/commands/misc/avatar.js
+++ src/bot/commands/misc/avatar.js
@@ -1,24 +1,25 @@
-// Shows the avatar of the user provided
+// Shows the server avatar of the user provided
 
 import {Command} from 'yuuko';
-import {parseUser} from '../../util/discord';
+import {parseGuildMember} from '../../util/discord';
 
 const command = new Command('avatar', async (msg, args) => {
 	const MAX_AVATAR_IMAGE_SIZE = 512;
 
 	if (args.length === 0) {
-		msg.channel.createMessage(msg.author.dynamicAvatarURL('', MAX_AVATAR_IMAGE_SIZE)).catch(() => {});
+		msg.channel.createMessage(`${msg.member.avatarURL.split('?size=')[0]}?size=${MAX_AVATAR_IMAGE_SIZE}`).catch(() => {});
 		return;
 	}
-	const [member] = await parseUser(args.join(' '), msg.channel.guild, msg.author);
+	const [member] = await parseGuildMember(args[0], msg.channel.guild, msg.member);
 	if (!member) {
 		msg.channel.createMessage('Member not found, check if what you\'re using is correct or try using an ID.').catch(() => {});
 		return;
 	}
-	msg.channel.createMessage(member.dynamicAvatarURL('', MAX_AVATAR_IMAGE_SIZE)).catch(() => {});
+
+	msg.channel.createMessage(`${member.avatarURL.split('?size=')[0]}?size=${MAX_AVATAR_IMAGE_SIZE}`).catch(() => {});
 });
 command.help = {
 	args: '[user]',
-	desc: 'Shows your avatar, or the avatar of the given user.',
+	desc: 'Shows your server avatar, or the server avatar of the given user.',
 };
 export default command;
---
+++ src/bot/commands/misc/defaultavatar.js
@@ -0,0 +1,25 @@
+// Shows the default avatar of the user provided
+
+import {Command} from 'yuuko';
+import {parseUser} from '../../util/discord';
+
+const command = new Command('defaultavatar', async (msg, args) => {
+	const MAX_AVATAR_IMAGE_SIZE = 512;
+
+	if (args.length === 0) {
+		msg.channel.createMessage(msg.author.dynamicAvatarURL('', MAX_AVATAR_IMAGE_SIZE)).catch(() => {});
+		return;
+	}
+	const [user] = await parseUser(args[0], msg.channel.guild, msg.author);
+	if (!user) {
+		msg.channel.createMessage('Member not found, check if what you\'re using is correct or try using an ID.').catch(() => {});
+		return;
+	}
+
+	msg.channel.createMessage(user.dynamicAvatarURL('', MAX_AVATAR_IMAGE_SIZE)).catch(() => {});
+});
+command.help = {
+	args: '[user]',
+	desc: 'Shows your default avatar, or the default avatar of the given user.',
+};
+export default command;
